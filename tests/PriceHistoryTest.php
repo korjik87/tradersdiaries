97231f105181194846026e5a09fd771da964b4a4
Sergey <sprakapovich@buffergroup.com>
2024-03-17 14:07:36 +0300

    readme md

---
+++ tests/PriceHistoryTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class PriceHistoryTestCase extends TestCase
+{
+    public function testPrice(): void
+    {
+        $p = new ETHBTCPriceHistory();
+    }
+}
